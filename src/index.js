26f71e3483c49d7ae28e4f4dd7e25886b405f414
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-22 16:51:24 -0300

    Added the buttons

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+const btnHome = document.querySelector("btn-home");
+const btnMenu = document.querySelector("btn-menu");
+const btnAbout = document.querySelector("btn-about");
+
+btnHome.addEventListener('click', () => {
+    console.log('Me clicked')
+});
