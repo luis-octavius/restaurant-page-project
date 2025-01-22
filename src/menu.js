aab5ac65465fbb4125cd32c1c44e1e081479545d
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-22 19:44:48 -0300

    I'm trying to make a class that generates a div that creates a card for a plate

--- src/menu.js
+++ src/menu.js
@@ -11,10 +11,33 @@ imgMenu1.src = img1;
 imgMenu2.src = img2;
 imgMenu3.src = img3;
 
+class divPlate {
+    constructor(father, img, h2, text) {
+        this.father = father;
+        this.img = img;
+        this.h2 = h2;
+        this.text = text;
+    }
+    set divAccoplate (father) {
+        const divP = document.createElement("div");
+        return this.father.appendChild(`${this.img}${this.h2}${this.text}`);
+    }
+}
+
 
 export function menuContent () {
     divContent.innerHTML = "";
     divContent.appendChild(imgMenu1);
     divContent.appendChild(imgMenu2);
     divContent.appendChild(imgMenu3);
+
+    const imgPack = divContent.getElementsByTagName("img");
+
+    for (let i = 0; i < imgPack.length; i++) {
+        imgPack[i].style.cssText = "width: 150px; height: auto;"
+    }
+
+    const divPlate1 = new divPlate(divContent, )
+
+
 }
