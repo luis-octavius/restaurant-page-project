c2f7ed0e21e851768fb8b01b2a4ecda2e70cb689
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-22 19:21:53 -0300

    Added the images

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,20 @@
 import { divContent } from "./home.js";
+import img1 from "./assets/brookelark.jpg";
+import img2 from "./assets/brookelark2.jpg";
+import img3 from "./assets/odiseocastrejon.jpg";
+
+const imgMenu1 = new Image ();
+const imgMenu2 = new Image ();
+const imgMenu3 = new Image ();
+
+imgMenu1.src = img1;
+imgMenu2.src = img2;
+imgMenu3.src = img3;
+
 
 export function menuContent () {
-    
+    divContent.innerHTML = "";
+    divContent.appendChild(imgMenu1);
+    divContent.appendChild(imgMenu2);
+    divContent.appendChild(imgMenu3);
 }
