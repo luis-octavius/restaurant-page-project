d1bf140f4a30d0b832ea485f55a905c49455008d
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-24 19:22:38 -0300

    Put a footer in the page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import html from "./template.html";
 console.log(divContent);
 menuContent();
 
-
+const body = document.querySelector("body");
 const btnHome = document.querySelector("#btn-home");
 const btnMenu = document.querySelector("#btn-menu");
 const btnAbout = document.querySelector("#btn-about");
@@ -16,3 +16,13 @@ const btnAbout = document.querySelector("#btn-about");
 btnHome.addEventListener('click', homeContent);
 btnMenu.addEventListener('click', menuContent)
 
+const footer = document.createElement("footer");
+body.appendChild(footer);
+
+footer.style.cssText = "display: flex; align-items: center; margin: 0; border: 0;"
+
+const p = document.createElement("p");
+p.textContent = "Copyright © Luis Octávio";
+p.style.cssText = "font-family: 'Franklin Gothic Medium', sans-serif;"
+
+footer.appendChild(p);
