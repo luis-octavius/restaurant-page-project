3c5cd49c7fb9631742a2f5af40222652c2066f7c
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-25 00:34:05 -0300

    initiating the work on about section

---
+++ src/about.js
@@ -0,0 +1,10 @@
+import { divContent } from "./home";
+
+
+
+export function aboutContent () {
+    divContent.innerHTML = "";
+
+    const h1 = document.createElement("h1");
+    h1.classList.add('font-monoton');
+}
